da156c8af530fc3684abd253c8d0d33553650717
CedaAlPaso <96441315+printMelen@users.noreply.github.com>
2023-04-14 20:46:57 +0200

    miraquetedije

--- src/main/java/Entrega1/Ejercicio1.java
+++ src/main/java/Entrega1/Ejercicio1.java
@@ -5,12 +5,13 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Escribe 2 números: ");
+		System.out.println("Escribe 2 nÃºmeros: ");
 		int num1= sc.nextInt();
 		int num2= sc.nextInt();
 		String resultado = (num1 == num2)? "Son iguales" :"Son distintos";
 		System.out.println(resultado);
-		System.out.println();
+		System.out.println("Hola");
+		
 	}
 
 }
